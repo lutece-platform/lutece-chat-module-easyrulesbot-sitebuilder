6378ceca9d482d823d7a98218ebb79c1d4b409c3
Close <alexandre.close@paris.fr>
2016-05-18 11:38:19 +0200

    BOTSITEBLD-2 : Ajout de la gestion de la version du Core dans les propriétés du site

---
+++ src/sql/plugins/easyrulesbot/modules/sitebuilder/init_core_sitebuilder.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  closea
+ * Created: 17 mai 2016
+ */
+
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES
+	('module.easyrulesbot.sitebuilder.site_properties.name', 'Propriétés concernant la création du POM de site'),
+	('module.easyrulesbot.sitebuilder.site_properties.description', 'Ces propriétés permettent de préciser au robot comment construire le POM de site' ),
+        ('module.easyrulesbot.sitebuilder.site_property.coreversion','[5.0.0,)');
+
